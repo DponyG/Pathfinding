adfca90c34a293ac297598588e1880b29b3416f7
Darkpony <dponyg@gmail.com>
2019-04-10 17:02:09 -0800

    Pushing Walls

--- grid.py
+++ grid.py
@@ -3,17 +3,35 @@
 
 from variables import *
 import pygame as pg
+vec = pg.math.Vector2
 
 
 class SquareGrid:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, GRIDWIDTH, GRIDHEIGHT, screen, WIDTH, HEIGHT):
+        self.screen = screen
+        self.GRIDWIDTH = GRIDWIDTH
+        self.GRIDHEIGHT = GRIDHEIGHT
         self.walls = []
         self.connections = [vec(1, 0), vec(-1, 0), vec(0, 1), vec(0, -1)]
 
+        self.TILESIZE = 48
+        self.WIDTH = WIDTH
+        self.HEIGHT= HEIGHT
+
+
+        ##  COLOR CONSTANTS
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.RED = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.CYAN = (0, 255, 255)
+        self.MAGENTA = (255, 0, 255)
+        self.YELLOW = (255, 255, 0)
+        self.DARKGRAY = (40, 40, 40)
+        self.LIGHTGRAY = (140, 140, 140)
+  
     def in_bounds(self, node):
-        return 0 <= node.x < self.width and 0 <= node.y < self.height
+        return 0 <= node.x < self.GRIDWIDTH and 0 <= node.y < self.GRIDHEIGHT
 
     def passable(self, node):
         return node not in self.walls
@@ -26,11 +44,17 @@ class SquareGrid:
 
     def draw_wall(self):
         for wall in self.walls:
-            rect = pg.Rect(wall * TILESIZE, (TILESIZE, TILESIZE))
-            pg.draw.rect(screen, LIGHTGRAY, rect)
+            rect = pg.Rect(wall * self.TILESIZE, (self.TILESIZE, self.TILESIZE))
+            pg.draw.rect(self.screen, self.LIGHTGRAY, rect)
     
     def vec2int(v):
         return (int(v.x), int(v.y))
+
+    def draw_grid(self):
+        for x in range(0, self.WIDTH, self.TILESIZE):
+            pg.draw.line(self.screen, self.LIGHTGRAY, (x, 0), (x, self.HEIGHT))
+        for y in range(0, self.HEIGHT, self.TILESIZE):
+            pg.draw.line(self.screen, self.LIGHTGRAY, (0, y), (self.WIDTH, y))
 
 
 
--- hw1.py
+++ hw1.py
@@ -4,17 +4,28 @@ from os import path
 from variables import *
 from grid import SquareGrid
 
+TILESIZE = 48
+GRIDWIDTH = 28
+GRIDHEIGHT = 15
+WIDTH = TILESIZE*GRIDWIDTH
+HEIGHT= TILESIZE*GRIDHEIGHT
+DARKGRAY = (40, 40, 40)
+
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
-g = SquareGrid(GRIDWIDTH, GRIDHEIGHT)
+
+FPS = 30
+g = SquareGrid(GRIDWIDTH, GRIDHEIGHT, screen, WIDTH, HEIGHT)
+
+## Will be parsed by a file parser
+walls = [(10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (7, 7), (6, 7), (5, 7), (5, 5), (5, 6), (1, 6), (2, 6), (3, 6), (5, 10), (5, 11), (5, 12), (5, 9), (5, 8), (12, 8), (12, 9), (12, 10), (12, 11), (15, 14), (15, 13), (15, 12), (15, 11), (15, 10), (17, 7), (18, 7), (21, 7), (21, 6), (21, 5), (21, 4), (21, 3), (22, 5), (23, 5), (24, 5), (25, 5), (18, 10), (20, 10), (19, 10), (21, 10), (22, 10), (23, 10), (14, 4), (14, 5), (14, 6), (14, 0), (14, 1), (9, 2), (9, 1), (7, 3), (8, 3), (10, 3), (9, 3), (11, 3), (2, 5), (2, 4), (2, 3), (2, 2), (2, 0), (2, 1), (0, 11), (1, 11), (2, 11), (21, 2), (20, 11), (20, 12), (23, 13), (23, 14), (24, 10), (25, 10), (6, 12), (7, 12), (10, 12), (11, 12), (12, 12), (5, 3), (6, 3), (5, 4)]
+for wall in walls:
+    g.walls.append(vec(wall))
+
+start = vec(14, 8)
 
 
-def draw_grid():
-    for x in range(0, WIDTH, TILESIZE):
-        pg.draw.line(screen, LIGHTGRAY, (x, 0), (x, HEIGHT))
-    for y in range(0, HEIGHT, TILESIZE):
-        pg.draw.line(screen, LIGHTGRAY, (0, y), (WIDTH, y))
 
 def main():
     running = True
@@ -26,7 +37,7 @@ def main():
 
         pg.display.set_caption("{:.2f}".format(clock.get_fps()))
         screen.fill(DARKGRAY)
-        draw_grid()
+        g.draw_grid()
         g.draw_wall()
         pg.display.flip()
 
